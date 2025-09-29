cdd6599c568f1fb4bc73cccbb6572d17824584fb
Joachim <joachim.vogt@pm.me>
2025-09-29 15:59:52 +0200

    Built user functionality for giving feedback on GPT responses by providing the question, 'Fikk du svar på spørsmålet ditt?' and clickable thumbs up/down buttons. After clicking  upda tes the currently selected chat  with the corresponding thumbs up/down. A small and scalable icon API has been adde d - a folder to do the same for all buttons has also been added (but for future discussions). Lastly, some small ty po fixes where I spotted them.

--- app/skattegpt/page.tsx
+++ app/skattegpt/page.tsx
@@ -1,153 +1,203 @@
-'use client'
+"use client";
 import { useContext, useEffect, useState } from "react";
 import GptResponseDisplay from "../src/components/textManagement/markdownTextDisplay";
 import ParagraphsDisplay from "../src/components/textManagement/paragraphsDisplay";
-import { QueryChatRequest, SearchState } from "../src/interface/skattSokInterface";
+import {
+  QueryChatRequest,
+  SearchState,
+} from "../src/interface/skattSokInterface";
 import HistoryDropdownSelect from "../src/components/localStorage/historyDropdownSelect.tsx";
 import DeleteLocalStorage from "../src/components/localStorage/clearLocalStorage";
 import ToggleSwitch from "../src/components/toogleModelDepth";
 import DownloadCSV from "../src/components/textManagement/csvRapport";
+import { Icons } from "../src/components/icons/iconsWrapper";
 import UserContext from "../src/contexts/user";
 
-
 const initialSearchResponse: SearchState = {
-    searchInput: '',
-    queryResponse: '',
-    paragraphsResponse: [''],
+  id: "",
+  searchInput: "",
+  queryResponse: "",
+  paragraphsResponse: [""],
+  chatFeedback: null,
 };
 
 export default function Search() {
-    const [searchResponse, setSearchResponse] = useState<SearchState>(initialSearchResponse);
-    const [searchHistory, setSearchHistory] = useState<SearchState[]>([]);
-    const [isDetailed, setIsDetailed] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
-    const [searchInput, setSearchInput] = useState('');
-    const [isClearHistoryDisabled, setIsClearHistoryDisabled] = useState(true);
+  const [searchResponse, setSearchResponse] = useState<SearchState>(
+    initialSearchResponse
+  );
+  const [searchHistory, setSearchHistory] = useState<SearchState[]>([]);
+  const [isDetailed, setIsDetailed] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+  const [isClearHistoryDisabled, setIsClearHistoryDisabled] = useState(true);
 
-    const { user } = useContext(UserContext);
+  const { user } = useContext(UserContext);
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            const savedHistoryList = getSearchHistory();
-            setSearchHistory(savedHistoryList);
-            setIsClearHistoryDisabled(savedHistoryList.length === 0);
-            if (savedHistoryList.length > 0) {
-                const latestSearch = savedHistoryList[savedHistoryList.length - 1];
-                setSearchResponse(latestSearch);
-                setSearchInput(latestSearch.searchInput);
-            }
-    }}, []);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const savedHistoryList = getSearchHistory();
+      setSearchHistory(savedHistoryList);
+      setIsClearHistoryDisabled(savedHistoryList.length === 0);
+      if (savedHistoryList.length > 0) {
+        const latestSearch = savedHistoryList[savedHistoryList.length - 1];
+        setSearchResponse(latestSearch);
+        setSearchInput(latestSearch.searchInput);
+      }
+    }
+  }, []);
 
-    const saveSearchHistory = (newSearchResponse: SearchState) => {
-        const updatedHistoryList = [...searchHistory, newSearchResponse];
-        localStorage.setItem('searchHistory', JSON.stringify(updatedHistoryList));
-        setSearchHistory(updatedHistoryList);
-        setIsClearHistoryDisabled(updatedHistoryList.length === 0);
-    };
+  const saveSearchHistory = (newSearchResponse: SearchState) => {
+    const updatedHistoryList = [...searchHistory, newSearchResponse];
+    localStorage.setItem("searchHistory", JSON.stringify(updatedHistoryList));
+    setSearchHistory(updatedHistoryList);
+    setIsClearHistoryDisabled(updatedHistoryList.length === 0);
+  };
 
-    const getSearchHistory = (): SearchState[] => {
-        if (typeof window !== 'undefined') {
-            const history = localStorage.getItem('searchHistory');
-            return history ? JSON.parse(history) as SearchState[] : [];
-        }
-        return [];
-    };
+  const getSearchHistory = (): SearchState[] => {
+    if (typeof window !== "undefined") {
+      const history = localStorage.getItem("searchHistory");
+      return history ? (JSON.parse(history) as SearchState[]) : [];
+    }
+    return [];
+  };
 
-    const clearSearchHistory = () => {
-        localStorage.removeItem('searchHistory');
-        setSearchHistory([]);
-        setIsClearHistoryDisabled(true);
-    };
+  const clearSearchHistory = () => {
+    localStorage.removeItem("searchHistory");
+    setSearchHistory([]);
+    setIsClearHistoryDisabled(true);
+  };
 
-    const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setSearchInput(event.target.value);
-    };
+  const handleSearchInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSearchInput(event.target.value);
+  };
 
-    const handleToggle = (state: boolean) => {
-        setIsDetailed(state);
-    };
+  const handleToggle = (state: boolean) => {
+    setIsDetailed(state);
+  };
 
-    const handleHistorySelect = (selectedSearch: SearchState) => {
-        setSearchResponse(selectedSearch);
-        setSearchInput(selectedSearch.searchInput);
-    };
+  const handleHistorySelect = (selectedSearch: SearchState) => {
+    setSearchResponse(selectedSearch);
+    setSearchInput(selectedSearch.searchInput);
+  };
 
-    const handeButtonClick = async () => {
-        setIsLoading(true);
-        try {
-            console.log('API call -> searchText: ', searchInput);
-            const queryChatRequest: QueryChatRequest = {
-                searchText: searchInput,
-                isDetailed: isDetailed,
-                username: user?.username ? user.username : 'default',
-                history: getSearchHistory()
-            }
+  const handleButtonClick = async () => {
+    setIsLoading(true);
+    try {
+      console.log("API call -> searchText: ", searchInput);
+      const queryChatRequest: QueryChatRequest = {
+        searchText: searchInput,
+        isDetailed: isDetailed,
+        username: user?.username ? user.username : "default",
+        history: getSearchHistory(),
+      };
 
-            const response = await fetch(`/api/query`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(queryChatRequest)
-            });
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const data = await response.json();
-            console.log('Client API call successful: ', data);
-            const newSearchResponse = {
-                searchInput: searchInput,
-                queryResponse: data.openaiResponse,
-                paragraphsResponse: data.esParagraphSearch
-            };
-            setSearchResponse(newSearchResponse);
-            saveSearchHistory(newSearchResponse); 
-            localStorage.setItem('searchResponse', JSON.stringify(newSearchResponse));
-        } catch (error) {
-            console.error('Error fetching search results:', error);
-        }
-        setIsLoading(false);
-    };
+      const response = await fetch(`/api/query`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(queryChatRequest),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log("Client API call successful: ", data);
+      const newSearchResponse = {
+        id: Date.now().toString(),
+        searchInput: searchInput,
+        queryResponse: data.openaiResponse,
+        paragraphsResponse: data.esParagraphSearch,
+      };
+      setSearchResponse(newSearchResponse);
+      saveSearchHistory(newSearchResponse);
+      localStorage.setItem("searchResponse", JSON.stringify(newSearchResponse));
+    } catch (error) {
+      console.error("Error fetching search results:", error);
+    }
+    setIsLoading(false);
+  };
 
-    const handleKeyPress = (event: React.KeyboardEvent) => {
-        if (event.key === 'Enter') {
-            handeButtonClick();
-        }
-    };
+  const handleChatFeedback = (feedback: "thumbsUp" | "thumbsDown") => {
+    const ratedChatResponse = { ...searchResponse, chatFeedback: feedback };
+    setSearchResponse(ratedChatResponse);
 
-    return (
-        <div className="pt-10">
-            <div className="flex justify-center">
-                <input
-                    type="text"
-                    placeholder="Spør meg om skatt"
-                    className="input input-bordered mr-10 w-full max-w-2xl m-1"
-                    value={searchInput}
-                    onChange={handleSearchInputChange}
-                    onKeyDown={handleKeyPress}
-                />
-            </div>
-            <div className="flex justify-center pt-5">
-                <ToggleSwitch 
-                    onToggle={handleToggle} 
-                    textA="Konkret" 
-                    textB="Detaljert" 
-                />
-                <HistoryDropdownSelect searchHistory={searchHistory} onSelect={handleHistorySelect}/>
-                <DeleteLocalStorage disabled={isClearHistoryDisabled} handleDelete={clearSearchHistory}/>
-                <button className="btn bg-sky-700 hover:bg-sky-800 text-white font-bold m-1 px-6 rounded mr-10" disabled={isLoading || searchInput === ""} onClick={handeButtonClick}>
-                    { isClearHistoryDisabled ? 'Nytt spørsmål' : 'Oppfølgingsspørsmål' }
-                </button>
-            </div>
-            <div className="divider"></div>
-            <div className="px-60">
-                {isLoading && <p className="text-center">Loading...</p>}
-                {!isLoading && searchResponse !== initialSearchResponse && (
-                    <div>
-                        <GptResponseDisplay searchResponse={searchResponse.queryResponse} />
-                        <div className="divider p-12">Relevante paragrafer</div>
-                        <ParagraphsDisplay paragraphs={searchResponse.paragraphsResponse} />
-                    </div>
-                )}
-            </div>
-        </div>
+    const currentSearchHistory = getSearchHistory();
+    const updatedSearchHistory = currentSearchHistory.map((item) =>
+      item.id === searchResponse.id ? ratedChatResponse : item
     );
+    localStorage.setItem("searchHistory", JSON.stringify(updatedSearchHistory));
+    setSearchHistory(updatedSearchHistory);
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      handleButtonClick();
+    }
+  };
+
+  return (
+    <div className="pt-10">
+      <div className="flex justify-center">
+        <input
+          type="text"
+          placeholder="Spør meg om skatt"
+          className="input input-bordered mr-10 w-full max-w-2xl m-1"
+          value={searchInput}
+          onChange={handleSearchInputChange}
+          onKeyDown={handleKeyPress}
+        />
+      </div>
+      <div className="flex justify-center pt-5">
+        <ToggleSwitch
+          onToggle={handleToggle}
+          textA="Konkret"
+          textB="Detaljert"
+        />
+        <HistoryDropdownSelect
+          searchHistory={searchHistory}
+          onSelect={handleHistorySelect}
+        />
+        <DeleteLocalStorage
+          disabled={isClearHistoryDisabled}
+          handleDelete={clearSearchHistory}
+        />
+        <button
+          className="btn bg-sky-700 hover:bg-sky-800 text-white font-bold m-1 px-6 rounded mr-10"
+          disabled={isLoading || searchInput === ""}
+          onClick={handleButtonClick}
+        >
+          {isClearHistoryDisabled ? "Nytt spørsmål" : "Oppfølgingsspørsmål"}
+        </button>
+      </div>
+      <div className="divider"></div>
+      <div className="px-60">
+        {isLoading && <p className="text-center">Loading...</p>}
+        {!isLoading && searchResponse !== initialSearchResponse && (
+          <div>
+            <GptResponseDisplay searchResponse={searchResponse.queryResponse} />
+
+            <div className="divider"></div>
+            <div className="flex justify-start pr-10 pt-5">
+              <p className="pt-3 pr-3">Fikk du svar på spørsmålet ditt?</p>
+              <button
+                className="btn btn-sm bg-green-600 hover:bg-green-700 text-white m-1 px-6 rounded"
+                onClick={() => handleChatFeedback("thumbsUp")}
+              >
+                <Icons.ThumbsUp className="w-5 h-5" />
+              </button>
+              <button
+                className="btn btn-sm bg-red-600 hover:bg-red-700 text-white  m-1 px-6 rounded"
+                onClick={() => handleChatFeedback("thumbsDown")}
+              >
+                <Icons.ThumbsDown className="w-5 h-5" />
+              </button>
+            </div>
+            <div className="divider p-12">Relevante paragrafer</div>
+            <ParagraphsDisplay paragraphs={searchResponse.paragraphsResponse} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
--- app/src/components/localStorage/historyDropdownSelect.tsx.tsx
+++ app/src/components/localStorage/historyDropdownSelect.tsx.tsx
@@ -1,15 +1,18 @@
 // historyDropdownSelect.tsx
-'use client';
-import React from 'react';
-import { SearchState } from '../../interface/skattSokInterface';
+"use client";
+import React from "react";
+import { SearchState } from "../../interface/skattSokInterface";
+import { Icons } from "../icons/iconsWrapper";
 
 interface HistoryDropdownProps {
   searchHistory: SearchState[];
   onSelect: (search: SearchState) => void;
 }
 
-const HistoryDropdownSelect: React.FC<HistoryDropdownProps> = ({ searchHistory, onSelect }) => {
-
+const HistoryDropdownSelect: React.FC<HistoryDropdownProps> = ({
+  searchHistory,
+  onSelect,
+}) => {
   const handleItemClick = (selectedSearch: SearchState) => {
     onSelect(selectedSearch);
   };
@@ -19,19 +22,35 @@ const HistoryDropdownSelect: React.FC<HistoryDropdownProps> = ({ searchHistory, 
   return (
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn m-1 px-6 rounded mr-10">
-        {"Spørsmål logg"}
+        {"Logg"}
       </div>
-        <ul className="dropdown-content z-[1] menu p-2 shadow bg-base-300 rounded-box w-64 max-h-200 overflow-y-auto">
-          {reversedHistory.length > 0 ? (
-            reversedHistory.map((search, index) => (
-              <li key={index}>
-                <a href="#" onClick={() => handleItemClick(search)}>{`${search.searchInput}`}</a>
-              </li>
-            ))
-          ) : (
-            <li><a>Ingen historikk</a></li>
-          )}
-        </ul>
+      <ul className="dropdown-content z-[1] menu p-2 shadow bg-base-300 rounded-box w-64 max-h-200 overflow-y-auto">
+        {reversedHistory.length > 0 ? (
+          reversedHistory.map((search, index) => (
+            <li key={index}>
+              <a
+                href="#"
+                onClick={() => handleItemClick(search)}
+                className="flex justify-between items-center w-full"
+              >
+                <span>{search.searchInput}</span>
+                <span>
+                  {search.chatFeedback === "thumbsUp" && (
+                    <Icons.ThumbsUp className="w-4 h-4 text-green-600" />
+                  )}
+                  {search.chatFeedback === "thumbsDown" && (
+                    <Icons.ThumbsDown className="w-4 h-4 text-red-600" />
+                  )}
+                </span>
+              </a>
+            </li>
+          ))
+        ) : (
+          <li>
+            <a>Ingen historikk</a>
+          </li>
+        )}
+      </ul>
     </div>
   );
 };
