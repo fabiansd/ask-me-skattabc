e502dce4f93f97e5cc062e4d86ddd2399a34ec1f
Joachim <joachim.vogt@pm.me>
2025-09-29 15:59:52 +0200

    Built user functionality for giving feedback on GPT responses by providing the question, 'Fikk du svar på spørsmålet ditt?' and clickable thumbs up/down buttons. After clicking  upda tes the currently selected chat  with the corresponding thumbs up/down. A small and scalable icon API has been adde d - a folder to do the same for all buttons has also been added (but for future discussions). Lastly, some small ty po fixes where I spotted them.

--- app/src/interface/skattSokInterface.ts
+++ app/src/interface/skattSokInterface.ts
@@ -1,13 +1,14 @@
-
 export interface SearchState {
-    searchInput: string;
-    queryResponse: string;
-    paragraphsResponse: string[];
+  id: string;
+  searchInput: string;
+  queryResponse: string;
+  paragraphsResponse: string[];
+  chatFeedback?: "thumbsUp" | "thumbsDown" | null;
 }
 
 export interface QueryChatRequest {
-    searchText: string,
-    isDetailed: boolean,
-    username: string,
-    history: SearchState[],
+  searchText: string;
+  isDetailed: boolean;
+  username: string;
+  history: SearchState[];
 }
---
+++ notes.md
@@ -0,0 +1,9 @@
+### Developer Joachim Vogt's notes
+
+#### Questions
+
+- What is next.js, does it need to be installed?
+
+#### Tech in need of dev research
+
+#### TODO's
